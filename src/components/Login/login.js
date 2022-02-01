a2937ca0c86f9eab8314ca7a552eb8ec3b6a300a
jdacunha <dacunha.jason@gmail.com>
2022-02-01 14:48:54 +0100

    add login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,13 +1,73 @@
 import { useState } from "react";
 import TextField from '@mui/material/TextField';
+import jwtDecode from "jwt-decode";
+import { Route } from 'react-router-dom';
 
-function login() {
+/* function login() {
     return(
       <div className='login'>
         <TextField id="outlined-basic" label="Username" variant="outlined" />
         <TextField id="outlined-basic" label="Password" variant="outlined" />
       </div>
     );
+  }; */
+
+function Login() {
+  const [values, setValues] = useState({ username: "", password: "" });
+  const [user, setUser] = useState(localStorage.getItem("token"));
+  function handleChange(e) {
+    setValues({ ...values, [e.target.name]: e.target.value });
   }
+  // VanillaJS approach (Closure)
+  //function handleChange(name) {
+  //  return function (e) {
+  //    setValues({ ...values, [name]: e.target.value });
+  //  };
+  //}
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://fauques.freeboxos.fr:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        setUser(data.token);
+        console.log(jwtDecode(data.token));
+      });
+  }
+  return user ? (
+    <><span>Already Logged</span><button onClick={Logout}> Logout </button></>
+    //<span> Already logged</span>
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <input name="username" value={values.username} onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      <input type="submit" value="Login" />
+    </form>
+  );
+}
+
+function Logout() {
+  localStorage.clear('token');
+
+  <Route render={({ history}) => (
+      history.push("/")
+    )}> </Route>
+
+    window.location.href = '/';
+
+} 
+
   
-  export default login;
+  export default Login;
