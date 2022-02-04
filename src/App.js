93742be81b49e38bc641af0873b324a84a91f63f
P-NAGANATHAN <piravin95400@gmail.com>
2022-02-04 10:35:29 +0100

    5rd commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React, { Component } from 'react';
-import "./images/ciseaux.png"
-import "./images/feuille.png"
-import "./images/pierre.png"
+import HomeIcon from '@mui/icons-material/Home';
+import ListIcon from '@mui/icons-material/List';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { Link, Route, Routes } from "react-router-dom";
 //import Test from "./components/Test/test2";
-import MatchList from "./components/Matches/MatchList";
+import MatchList, {Test} from "./components/Matches/MatchList";
 import Login from "./components/Login/Login";
-import Shifumi from "./components/Game/Shifumi"
-import Test from "./components/Test/test2";
+import PierreFeuilleCiseaux from "./components/Game/PierreFeuilleCiseaux"
+//import Test from "./components/Test/test2";
 
 
 
@@ -20,21 +21,19 @@ class App extends Component {
     <div className="App">
       <header className="App-header">
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/test">Test</Link>
-          <Link to="/test2">Test</Link>
-          <Link to="/matchs">Matchs</Link>
+        <Link to="/"> <HomeIcon> </HomeIcon> <span> Home </span> </Link> 
+          <Link to="/test"> <SettingsIcon></SettingsIcon> <span> Shifumi Test </span> </Link>
+          <Link to="/matchs"> <ListIcon> </ListIcon> <span> Matchs </span> </Link>
+          <Link to="/login" className='BoutonLogin'> <AccountCircleIcon> </AccountCircleIcon> <span> Login </span> </Link>
           <Link to="/jeu">Game Test</Link>
         </nav>
+        </header>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/test" element={<Shifumi />} />
-          <Route path="/test2" element={<Test />} />
-          <Route path="/matchs" element={<MatchList />} />
+          <Route path="/test" element={<PierreFeuilleCiseaux />} />
+          <Route path="/matchs" element={<><MatchList/><Test/></>} />
           <Route path="/" element={<Home />} />
         </Routes>
-      </header>
     </div>
   );
 }
--- src/components/Game/Shifumi.js
+++ src/components/Game/Shifumi.js
@@ -1,31 +1,6 @@
 function Test() {
 
 
-  function handleClick() {
-    
-    // Send data to the backend via POST
-    fetch('http://fauques.freeboxos.fr:3000/matches', {  
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-      method: 'POST', 
-      mode: 'cors'
-      
-    })
-    
-  }
-
-  return (
-    <div onClick={handleClick} style={{
-      textAlign: 'center',
-      width: '100px',
-      border: '1px solid gray',
-      borderRadius: '5px'
-    }}>
-      Send data to backend
-    </div>
-  );
-
 }
 
 export default Test;
